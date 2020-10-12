7cb5e1c8082f02b3cb344cfc3d375e57585bb017
angelapetruseva <58328846+angelapetruseva@users.noreply.github.com>
2020-10-13 01:08:22 +0200

    Updates

---
+++ precache-manifest.646769775882d4b811a398126748e779.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3943973e18d50206b1b148797f5c83",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "22921078975033abee34",
+    "url": "/covid-19/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "44c1e51e848255689657",
+    "url": "/covid-19/static/css/main.b4be8078.chunk.css"
+  },
+  {
+    "revision": "22921078975033abee34",
+    "url": "/covid-19/static/js/2.dba2dacd.chunk.js"
+  },
+  {
+    "revision": "4469949a898bb113ec37741ef9f4b742",
+    "url": "/covid-19/static/js/2.dba2dacd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c1e51e848255689657",
+    "url": "/covid-19/static/js/main.c7c214b1.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  },
+  {
+    "revision": "c5840695e40679d0816208a9d0f6ab22",
+    "url": "/covid-19/static/media/alcohol-gel.c5840695.png"
+  },
+  {
+    "revision": "0e5463784d7a8b34e1cb7ccd0f6c670b",
+    "url": "/covid-19/static/media/breathing.0e546378.png"
+  },
+  {
+    "revision": "69285175489d466ede00aa7df801ffdd",
+    "url": "/covid-19/static/media/cough.69285175.png"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/cover.d7265326.png"
+  },
+  {
+    "revision": "31e7bba0d00ad96f8bb591230d7678f0",
+    "url": "/covid-19/static/media/distance.31e7bba0.png"
+  },
+  {
+    "revision": "0077638baacf7f7d770974d34a5d650b",
+    "url": "/covid-19/static/media/fatigue.0077638b.png"
+  },
+  {
+    "revision": "291d0bef5f5fb050999c306d0098d3f1",
+    "url": "/covid-19/static/media/fever.291d0bef.png"
+  },
+  {
+    "revision": "4df56b3e8a3a07e580a3a6d1c235f09e",
+    "url": "/covid-19/static/media/protection-mask.4df56b3e.png"
+  },
+  {
+    "revision": "f023e1bfe0f87cddb3e7fb0c6132c443",
+    "url": "/covid-19/static/media/soap.f023e1bf.png"
+  }
+]);
