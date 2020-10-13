d1287afc562db5af0f8701e14696cb4e6108ea95
angelapetruseva <58328846+angelapetruseva@users.noreply.github.com>
2020-10-13 02:16:25 +0200

    Updates

---
+++ precache-manifest.1de766a0c45ab1e89de573430a0f2233.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e7a7ae1640891b836d7b630dc35205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04b351918b639247d651",
+    "url": "/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "abf3c1f844ec54bc0ba2",
+    "url": "/static/css/main.b4be8078.chunk.css"
+  },
+  {
+    "revision": "04b351918b639247d651",
+    "url": "/static/js/2.2155e957.chunk.js"
+  },
+  {
+    "revision": "4469949a898bb113ec37741ef9f4b742",
+    "url": "/static/js/2.2155e957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf3c1f844ec54bc0ba2",
+    "url": "/static/js/main.c5c28957.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  },
+  {
+    "revision": "c5840695e40679d0816208a9d0f6ab22",
+    "url": "/static/media/alcohol-gel.c5840695.png"
+  },
+  {
+    "revision": "0e5463784d7a8b34e1cb7ccd0f6c670b",
+    "url": "/static/media/breathing.0e546378.png"
+  },
+  {
+    "revision": "69285175489d466ede00aa7df801ffdd",
+    "url": "/static/media/cough.69285175.png"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/cover.d7265326.png"
+  },
+  {
+    "revision": "31e7bba0d00ad96f8bb591230d7678f0",
+    "url": "/static/media/distance.31e7bba0.png"
+  },
+  {
+    "revision": "0077638baacf7f7d770974d34a5d650b",
+    "url": "/static/media/fatigue.0077638b.png"
+  },
+  {
+    "revision": "291d0bef5f5fb050999c306d0098d3f1",
+    "url": "/static/media/fever.291d0bef.png"
+  },
+  {
+    "revision": "4df56b3e8a3a07e580a3a6d1c235f09e",
+    "url": "/static/media/protection-mask.4df56b3e.png"
+  },
+  {
+    "revision": "f023e1bfe0f87cddb3e7fb0c6132c443",
+    "url": "/static/media/soap.f023e1bf.png"
+  }
+]);
